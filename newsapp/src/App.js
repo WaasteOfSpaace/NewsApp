e99134df0c1d2a5fed0238dbdf398f75857d8d82
WaasteOfSpaace <john.stewartrussell@outlook.com>
2021-02-18 15:52:56 +1300

    created react app

---
+++ newsapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import CompoOne from './components/CompoOne';
+
+
+function App() {
+  return (
+    <div className="App">
+    <CompoOne/>
+    </div>
+  );
+}
+
+export default App;
---
+++ newsapp/src/components/CompoOne.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+
+export default class CompoOne extends Component {
+    render() {
+        return (
+            <div>
+                <h1>HELLO WORLD</h1>
+            </div>
+        )
+    }
+}
